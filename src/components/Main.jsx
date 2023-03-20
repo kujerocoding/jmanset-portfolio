4ef5ee4f1037c8c9d7519a671c0b069bf7578425
herom <jerome.manset22@gmail.com>
2023-03-21 07:27:58 +0800

    Add styles to the button elements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react'
-import ProjectDetails from './ProjectDetails'
 import ProjectData from './ProjectData';
+import {BsCart4, BsTiktok} from 'react-icons/bs'
+import {MdWeb} from 'react-icons/md'
+
 
 
 const Main = () => {
 
     const [data, setData] = useState(ProjectData)
     const [showHello, setShowHello] = useState(true);
-    const [showProject, setShowProject] = useState(false);
 
     const handleClick = (e) => {
       const { name } = e.target
         console.log(e.target.name)
         setShowHello(false)
-        setShowProject(true)
         setData(prevData => {
             return prevData.map(item => item.name === name ? {...item, isOpen: true} : {...item, isOpen: false})
           }
         )
         //console.log(data)
     }
-    console.log('raw', data)
+    //console.log('raw', data)
     const filteredData = data.filter(item => item.isOpen === true)
-    console.log('filtered data: ',filteredData)
+    //console.log('filtered data: ',filteredData)
     const {title, img, desc} = filteredData[0]
-    console.log(title,img,desc)
+    //console.log(title,img,desc)
 
   return (
     <main>
@@ -32,13 +32,13 @@ const Main = () => {
     <div className='info-container'>
        
         {showHello && <div>
-            <h1>Hello!</h1>
+            <h1 className='title-hello'>Hello!</h1>
             <p>
                 Career shifter with a background in X (your previous field) and a passion for web development. Strong problem-solver with a keen eye for detail and a drive to stay current with the latest web development technologies. Proven ability to learn quickly and work well in a team.
             </p>
         </div>}
         
-       { showProject && <div className='info-project-container'>
+       { !showHello && <div className='info-project-container'>
             
             <div className='img-container'>
                 <h3>{title}</h3>
@@ -58,9 +58,9 @@ const Main = () => {
     <div className='project-container'>
         <h2 className='project-title'>Projects</h2>
         <ul className='project-lists'>
-            <li><button name="atv" onClick={handleClick}>Project 1</button></li>
-            <li><button name="case" onClick={handleClick}>Project 2</button></li>
-            <li><button name="mouse" onClick={handleClick}>Project 3</button></li>
+            <li><button name="atv" className='project-btn' onClick={handleClick}><span><BsCart4 /> |</span> Gaming Peripherals Online Store</button></li>
+            <li><button name="case" className='project-btn' onClick={handleClick}><span><MdWeb /> |</span> Tourist Destination Landing Page</button></li>
+            <li><button name="mouse" className='project-btn' onClick={handleClick}><span><BsTiktok /> |</span> Tiktok Clone</button></li>
         </ul>
     </div>
     </main>
