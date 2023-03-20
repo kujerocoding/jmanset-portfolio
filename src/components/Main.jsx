f72d7e54b8379d65652e2f89752632c0cbcf3c1b
herom <jerome.manset22@gmail.com>
2023-03-21 04:54:36 +0800

    Update media query

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,10 +6,14 @@ import ProjectData from './ProjectData';
 const Main = () => {
 
     const [data, setData] = useState(ProjectData)
+    const [showHello, setShowHello] = useState(true);
+    const [showProject, setShowProject] = useState(false);
 
     const handleClick = (e) => {
       const { name } = e.target
         console.log(e.target.name)
+        setShowHello(false)
+        setShowProject(true)
         setData(prevData => {
             return prevData.map(item => item.name === name ? {...item, isOpen: true} : {...item, isOpen: false})
           }
@@ -17,7 +21,6 @@ const Main = () => {
         console.log(data)
     }
 
-const [showHello] = useState(false);
   return (
     <main>
     <div className='info-container'>
@@ -27,10 +30,10 @@ const [showHello] = useState(false);
                 Career shifter with a background in X (your previous field) and a passion for web development. Strong problem-solver with a keen eye for detail and a drive to stay current with the latest web development technologies. Proven ability to learn quickly and work well in a team.
             </p>
         </div>}
-        <h3>Title</h3>
-        <div className='info-project-container'>
-            
-            <div className='img-container'><img src="../src/assets/image/atv.jpg"></img></div>
+        
+       { showProject && <div className='info-project-container'>
+
+            <div className='img-container'><img src=""></img></div>
             <div className='desc-container'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dolores voluptatem asperiores, eligendi labore nam sunt nulla nostrum atque? Quaerat praesentium iste commodi natus placeat enim incidunt laboriosam id quasi!</p>
                 <ul>
@@ -39,7 +42,7 @@ const [showHello] = useState(false);
                 </ul>
             </div>
             
-        </div>
+        </div>}
     </div>
     <div className='spacer'></div>
     <div className='project-container'>
@@ -55,3 +58,6 @@ const [showHello] = useState(false);
 }
 
 export default Main
+
+
+//../src/assets/image/atv.jpg
