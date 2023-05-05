865b269bea7ba7838c301c4a7d1a63cf202ebe28
herom <jerome.manset22@gmail.com>
2023-05-05 10:56:06 +0800

    Add stagger animation to the heading text and about me

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -5,36 +5,44 @@ import React from 'react'
 const Greetings = () => {
 
     const headingText = "Hi, I'm Jerome Manset."
-    const arrayText = headingText.split(' ')
-    const lettersHeading = Array.from(headingText)
-    console.log(headingText)
-    console.log(arrayText)
-    console.log(lettersHeading)
+    const headingLetters = Array.from(headingText)
 
-    const aboutMeText = "I am a self-taught front-end developer with a background in network engineering. I shifted my career to pursue my passion for web development, and I have been learning and building projects on my own. I am excited to continue growing my skills and contributing to the field."
-    const arrayAboutMeText = aboutMeText.split(' ')
+    const aboutMeText = "I am a self-taught front-end developer with a background in network engineering. I shifted my career to pursue my passion for web development, and I have been learning and building projects on my own. I am excited to continue growing my skills and contributing to the field. In my free time, I enjoy watching NBA games and playing online games."
+    const aboutMeWords = aboutMeText.split(".")
 
+    console.log(aboutMeWords)
+
+    const parentLetters = {
+        hidden:{opacity: 0},
+        visible: (i = 1) => ({
+            opacity: 1,
+            transition: { staggerChildren: 0.1, delayChildren: 0.02 * i} 
+        })
+    }
+    const parentWords = {
+        hidden:{opacity: 0},
+        visible: (i = 50) => ({
+            opacity: 1,
+            transition: { staggerChildren: 0.12, delayChildren: 0.04 * i, staggerDirection: -1} 
+        })
+    }
 
   return (
     <div className='hello-container b-green'>
-        <motion.div style={ {overflow: "hidden", display: "flex", flexWrap:"wrap"}}
+
+        <motion.div className='header--greetings b-blue'
             initial="hidden"
             animate="visible"
-            variants={{
-                hidden:{opacity: 0},
-                visible: (i = 1) => ({
-                    opacity: 1,
-                    transition: { staggerChildren: 0.12, delayChildren: 0.04 * i} 
-                })
-             }}
+            variants={parentLetters}
             >
-                {lettersHeading.map((letter, i) => (
+                {headingLetters.map((letter, i) => (
                     <motion.p key={i}
                    className='title-hello'
                     variants={{
                         hidden:{
                             opacity: 0,
-                            x: -20,
+                            x: i % 2 === 0 ? -50 : 50,
+                            y: -50,
                             transition: {
                                 type: "spring",
                                 damping: 12,
@@ -44,6 +52,7 @@ const Greetings = () => {
                         visible:{
                             opacity: 1,
                             x: 0,
+                            y: 0,
                             transition: {
                                 type: "spring",
                                 damping: 12,
@@ -54,40 +63,41 @@ const Greetings = () => {
                     >{letter === " " ? "\u00A0" : letter}</motion.p>
                 ))}
 
-                {arrayAboutMeText.map((word, i) => (
-                    <motion.span key={i}
-                   className=''
-                    variants={{
-                        hidden:{
-                            opacity: 0,
-                            x: 20,
-                            transition: {
-                                type: "spring",
-                                damping: 12,
-                                stiffness: 100
-                            }
-                        },
-                        visible:{
-                            opacity: 1,
-                            x: 0,
-                            transition: {
-                                type: "spring",
-                                damping: 12,
-                                stiffness: 100
-                            }
-                        }
-                    }}
-                    >{word}</motion.span>
-                ))}
 
         </motion.div>
-            
 
-            <p>
-            
-            <br />
-            In my free time, I enjoy watching NBA games and playing online games."
-            </p>
+        <motion.div
+            className='aboutme b-yellow'
+            initial="hidden"
+            animate="visible"
+            variants={parentWords}
+        >
+           {aboutMeWords.map((word, i) => (
+            <motion.p
+                className='' 
+                key={i}
+                variants={{
+                 hidden:{
+                     opacity: 0,
+                     y: 50,
+                     transition: {
+                         type: "spring",
+                         damping: 12,
+                         stiffness: 50
+                     }
+                 },
+                 visible:{
+                     opacity: 1,
+                     y: 0,
+                     transition: {
+                         type: "spring",
+                         damping: 12,
+                         stiffness: 50,
+                     }
+                 }
+             }}>{word}</motion.p>
+           ))}
+        </motion.div>
     </div>
   )
 }
